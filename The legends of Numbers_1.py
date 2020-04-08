e19425028d7a28c805ae468585975168c9a48d96
Kiryonn <60699390+Kiryonn@users.noreply.github.com>
2020-04-08 22:01:07 +0200

    add comments and animation

--- The legends of Numbers_1.py
+++ The legends of Numbers_1.py
@@ -5,8 +5,7 @@ Created on Thu Apr  2 17:59:24 2020
 @author: Kiryonn
 """
 
-from biblio import *
-from tkinter import Tk, Frame, Canvas, Button, Label
+from tkinter import Tk, Frame, Canvas, PhotoImage
 from tkinter.font import Font
 from pathlib import Path
 
@@ -33,24 +32,48 @@ class App(Tk):
         self._frame.pack(fill="both")
 
 class MainMenu(Frame):
+    """
+    Ecran d'acceuil
+    """
     def __init__(self, master):
-        self.master = master
-        self.isflashing = True
-        self.state = True
         Frame.__init__(self, master)
-        self.bgImage = PhotoImage(file="img/default.png")
-        self.cnv = Canvas(self, width=WIDTH, height=HEIGHT, bg="black")
-        self.cnv.pack()
         fontTitle = Font(family="Arial", size=40)
         fontNormal = Font(family="Arial", size=20)
+        self.master = master
+
+        # existe principalement pour éviter un messge d'erreur du a la fonction récurssive flash
+        self.isflashing = True
+
+        # state : True: montre le texte, False : desactive le texte
+        self.state = True
+
+        # création du canvas
+        self.cnv = Canvas(self, width=WIDTH, height=HEIGHT, bg="black")
+        self.cnv.pack()
+
+        # création de l'arriere plan
+        self.bgImage = PhotoImage(file="img/default2.png")
+        self.color = "#ffffff"
+        self.cnv.create_rectangle(0, 0, WIDTH, HEIGHT, fill=self.color)
+        self.cnv.create_image(WIDTH//2, HEIGHT//2, image=self.bgImage, anchor="center")
+        self.cnv.bgImage = self.bgImage
+
+        # ajout du titre du jeu
         self.cnv.create_text(WIDTH//2, 100, text="The Legends of Number :\n1", fill="green", font=fontTitle, justify="center")
-        self.text = self.cnv.create_text(WIDTH//2, HEIGHT*3//4, text="Appuyez sur Entrer pour jouer", disabledfill="#888888", fill="white", font=fontNormal)
+
+        # ajout de text informatif
+        self.text = self.cnv.create_text(WIDTH//2, HEIGHT*3//4, text="Appuyez sur Entrée pour jouer", disabledfill="#888888", fill="white", font=fontNormal)
+
+        # lancement de l'animation
         self.flash()
+        self.animation()
+
+        # évenements
         master.bind('<Return>', self.startCommand)
 
     def startCommand(self, event):
-        self.master.unbind("<Return>")
-        self.isflashing = False
+        self.master.unbind("<Return>") # par sécurité
+        self.isflashing = False # pour éviter un message d'erreur
         self.master.switch_frame(SelectMenu)
 
     def flash(self):
@@ -60,58 +83,77 @@ class MainMenu(Frame):
         else:
             self.cnv.itemconfig(self.text, state=("normal"))
             self.state = True
-        if self.isflashing:
+        if self.isflashing: # seulement si on ne se prépare pas a quitter l'écran pour éviter un message d'erreur
             self.after(400, self.flash)
 
+    def animation(self):
+        self.color = self.color # temporaire le temps que je trouve de quoi faire des belles couleurs
+        if self.isflashing:
+            self.after(10, self.animation)
+
 class SelectMenu(Frame):
+    """
+    Menu de selection/création de la partie
+    """
     def __init__(self, master):
         Frame.__init__(self, master)
+
+        # création du canvas
         self.cnv = Canvas(self, width=WIDTH, height=HEIGHT, bg="black")
         self.cnv.pack()
-        self.bgImage = PhotoImage(file="img/default.png")
+
+        # ajout des images
+        bgImage = PhotoImage(file="img/default.png")
         img = PhotoImage(file="img/select.png")
-        self.cnv.create_image(WIDTH//2, HEIGHT//2, image=self.bgImage, anchor="center")
+        self.cnv.create_image(WIDTH//2, HEIGHT//2, image=bgImage, anchor="center")
         self.cnv.create_image(WIDTH//2, HEIGHT//2, image=img, anchor="center")
-        self.cnv.bgImage = self.bgImage
+        self.cnv.bgImage = bgImage
         self.cnv.image = img
 
+        # récupération des infos des joueurs de chaques parties si existante
         self.infosFile1 = self.getFileInfo("saves/file1.txt")
         self.infosFile2 = self.getFileInfo("saves/file2.txt")
         self.infosFile3 = self.getFileInfo("saves/file3.txt")
 
-        # create buttons
-        self.b1 = self.ButtonMainMenu(self.cnv, WIDTH//4, 100, WIDTH//2, 75, text="Pas de sauvegarde" if self.infosFile1 == None else self.infosFile1["charaName"])
-        self.b2 = self.ButtonMainMenu(self.cnv, WIDTH//4, 200, WIDTH//2, 75, text="Pas de sauvegarde" if self.infosFile2 == None else self.infosFile2["charaName"])
-        self.b3 = self.ButtonMainMenu(self.cnv, WIDTH//4, 300, WIDTH//2, 75, text="Pas de sauvegarde" if self.infosFile3 == None else self.infosFile3["charaName"])
+        # création des boutons
+        self.b1 = Button(self.cnv, WIDTH//4, 100, WIDTH//2, 75, text="Pas de sauvegarde" if self.infosFile1 == None else self.infosFile1["charaName"])
+        self.b2 = Button(self.cnv, WIDTH//4, 200, WIDTH//2, 75, text="Pas de sauvegarde" if self.infosFile2 == None else self.infosFile2["charaName"])
+        self.b3 = Button(self.cnv, WIDTH//4, 300, WIDTH//2, 75, text="Pas de sauvegarde" if self.infosFile3 == None else self.infosFile3["charaName"])
         self.back = None
         self.play = None
 
-        # draw buttons
+        # affichage des boutons
         self.b1.draw()
         self.b2.draw()
         self.b3.draw()
 
-        # left click events
+        # création des évenements
+        # clique gauche
         self.b1.bind("<Button-1>", lambda e: self.func(e, 1))
         self.b2.bind("<Button-1>", lambda e: self.func(e, 2))
         self.b3.bind("<Button-1>", lambda e: self.func(e, 3))
 
-        # mouse over events
+        # souris au dessus de l'objet
         self.b1.bind("<Enter>", self.b1.mouseOver)
         self.b2.bind("<Enter>", self.b2.mouseOver)
         self.b3.bind("<Enter>", self.b3.mouseOver)
 
-        # mouse not over anymore events
+        # souris quitte le dessus de l'objet
         self.b1.bind("<Leave>", self.b1.mouseQuit)
         self.b2.bind("<Leave>", self.b2.mouseQuit)
         self.b3.bind("<Leave>", self.b3.mouseQuit)
 
     def getFileInfo(self, filepath):
+        """
+        E : le chemin d'acces d'un fichier
+        S : None : ssi pas d'infos sur la partie
+            infos(dico) : les infos de la partie ssi existantes
+        """
         infos = {
             "charaName" : "",
             "map" : (0, 0),
-            "life" : 3,
-            "maxLife" : 3}
+            "life" : 300,
+            "maxLife" : 300}
         try:
             file = open(filepath, "r")
             name = file.readline().strip('\n')
@@ -126,7 +168,9 @@ class SelectMenu(Frame):
             infos["maxLife"] = int(maxLife)
             return infos
         except EnvironmentError:
-            Path("saves").mkdir(parents=True, exist_ok=True)
+            Path("saves").mkdir(parents=True, exist_ok=True) # création du dossier si non existant
+            return None
+        except ValueError:
             return None
 
     def func(self, event, index):
@@ -135,9 +179,12 @@ class SelectMenu(Frame):
         else:
             print("this should not happen")
 
-    def transition1(self, index, cpt, dt=False):
+    def transition1(self, index, cpt, dt=1):
+        # cpt : compteur
+        # index : le numérau du bouton cliqué
+        # dt (détransition) : 1 si transition -1 si détransition
         if cpt == 0:
-            if dt:
+            if dt == -1:
                 self.b1.rebind()
                 self.b2.rebind()
                 self.b3.rebind()
@@ -145,114 +192,138 @@ class SelectMenu(Frame):
                 self.transition2(index, 10 if index==2 else 20)
         else:
             if index == 1:
-                self.b2.changePos(10 if dt else -10, 0)
-                self.b3.changePos(-10 if dt else 10, 0)
+                self.b2.changePos(-10*dt, 0)
+                self.b3.changePos(10*dt, 0)
             elif index == 2:
-                self.b1.changePos(10 if dt else -10, 0)
-                self.b3.changePos(-10 if dt else 10, 0)
+                self.b1.changePos(-10*dt, 0)
+                self.b3.changePos(10*dt, 0)
             else:
-                self.b1.changePos(10 if dt else -10, 0)
-                self.b2.changePos(-10 if dt else 10, 0)
+                self.b1.changePos(-10*dt, 0)
+                self.b2.changePos(10*dt, 0)
             self.after(10, lambda:self.transition1(index, cpt-1, dt=dt))
 
-    def transition2(self, index, cpt, dt=False):
+    def transition2(self, index, cpt, dt=1):
+        # cpt : compteur
+        # index : le numérau du bouton cliqué
+        # dt (détransition) : 1 si transition -1 si détransition
         if cpt == 0 or index == 1:
-            if dt:
-                self.transition1(index, 80, dt=True)
+            if dt == -1:
+                self.transition1(index, 80, dt=-1)
             else:
-                self.back = self.ButtonMainMenu(self.cnv, WIDTH//7, -40, 90, 40, text="Retour")
+                self.back = Button(self.cnv, WIDTH//7, -40, 90, 40, text="Retour")
+                self.play = Button(self.cnv, WIDTH//7, HEIGHT+40, 90, 40, text="Play")
+                self.b1.unbind()
                 self.transition3(index, 16)
         else:
             if index == 2:
-                self.b2.changePos(0, 10 if dt else -10)
+                self.b2.changePos(0, -10*dt)
             else:
-                self.b3.changePos(0, 10 if dt else -10)
+                self.b3.changePos(0, -10*dt)
             self.after(10, lambda:self.transition2(index, cpt-1, dt=dt))
 
-    def transition3(self, index, cpt):
+    def transition3(self, index, cpt, dt=1):
+        # cpt : compteur
+        # index : le numérau du bouton cliqué
+        # dt (détransition) : si vrai applique l'inverse de la transition
         if cpt == 0:
-            self.back.bind("<Button-1>", lambda e: self.detransition(index, event=e))
-            self.back.bind("<Enter>", self.back.mouseOver)
-            self.back.bind("<Leave>", self.back.mouseQuit)
-            self.showFileMenu(index)
+            if dt == -1:
+                self.transition2(index, 10 if index == 2 else 20, dt=-1)
+                self.back.destroy()
+                self.play.destroy()
+            else:
+                self.back.bind("<Button-1>", lambda e: self.detransition(index, e))
+                self.back.bind("<Enter>", self.back.mouseOver)
+                self.back.bind("<Leave>", self.back.mouseQuit)
+                self.showFileMenu(index)
         else:
-            self.back.changePos(0, +10)
-            self.after(10, lambda:self.transition3(index, cpt-1))
+            self.back.changePos(0, 10*dt)
+            self.play.changePos(0, -10*dt)
+            self.after(10, lambda:self.transition3(index, cpt-1, dt=dt))
 
-    def detransition(self, index, event=None):
-        self.back.destroy()
-        self.transition2(index, 10 if index==2 else 20, dt=True)
+    def detransition(self, index, event):
+        self.transition3(index, 10 if index==2 else 20, dt=-1)
 
     def showFileMenu(self, index):
         pass
 
+    def createFile(self, filepath):
+        pass
 
-    class ButtonMainMenu(object):
-        def __init__(self, canvas, x, y, width, height, marge=5, text=""):
-            self.cnv = canvas
-            self.marge = marge
-            self.points = [(x+marge,y), (x+width-marge, y), (x+width, y+marge), (x+width, y+height-marge), (x+width-marge, y+height), (x+marge, y+height), (x, y+height-marge), (x, y+marge)]
-            self.x = x
-            self.y = y
-            self.w = width
-            self.h = height
-            self.text = text
-            self.font = Font(family="Arial", size=20)
-            self.polygon = None
-            self.label = None
-            self.events = []
 
-        def draw(self):
-            self.polygon = self.cnv.create_polygon(self.points, outline="white", fill='')
-            self.label = self.cnv.create_text(self.x + self.w//2, self.y+self.h//2, text=self.text, font=self.font, fill="white")
+class Button(object):
+    """
+    Boutons custom
+    """
+    def __init__(self, canvas, x, y, width, height, marge=5, text="", font=None):
+        self.cnv = canvas
+        self.marge = marge
+        self.points = [(x+marge,y), (x+width-marge, y), (x+width, y+marge), (x+width, y+height-marge), (x+width-marge, y+height), (x+marge, y+height), (x, y+height-marge), (x, y+marge)]
+        self.x = x
+        self.y = y
+        self.w = width
+        self.h = height
+        self.text = text
+        self.font = Font(family="Arial", size=20) if font==None else font
+        self.polygon = None
+        self.label = None
+        self.events = []
 
-        def hide(self):
-            self.cnv.itemconfigure(self.polygon, state="hidden")
-            self.cnv.itemconfigure(self.label, state="hidden")
+    def draw(self):
+        self.polygon = self.cnv.create_polygon(self.points, outline="white", fill='')
+        self.label = self.cnv.create_text(self.x + self.w//2, self.y+self.h//2, text=self.text, font=self.font, fill="white")
 
-        def show(self):
-            self.cnv.itemconfigure(self.polygon, state="normal")
-            self.cnv.itemconfigure(self.label, state="normal")
+    def hide(self):
+        self.cnv.itemconfigure(self.polygon, state="hidden")
+        self.cnv.itemconfigure(self.label, state="hidden")
 
-        def destroy(self):
-            self.cnv.delete(self.polygon)
-            self.cnv.delete(self.label)
+    def show(self):
+        self.cnv.itemconfigure(self.polygon, state="normal")
+        self.cnv.itemconfigure(self.label, state="normal")
 
-        def changePos(self, x, y):
-            self.x += x
-            self.y += y
-            self.points = [(self.x+self.marge, self.y), (self.x+self.w-self.marge, self.y), (self.x+self.w, self.y+self.marge), (self.x+self.w, self.y+self.h-self.marge), (self.x+self.w-self.marge, self.y+self.h), (self.x+self.marge, self.y+self.h), (self.x, self.y+self.h-self.marge), (self.x, self.y+self.marge)]
-            self.cnv.delete(self.polygon)
-            self.cnv.delete(self.label)
-            self.polygon = self.cnv.create_polygon(self.points, outline="white", fill='')
-            self.label = self.cnv.create_text(self.x + self.w//2, self.y+self.h//2, text=self.text, font=self.font, fill="white")
+    def destroy(self):
+        self.cnv.delete(self.polygon)
+        self.cnv.delete(self.label)
 
-        def centerLabel(self):
-            self.cnv.delete(self.label)
-            self.label = self.cnv.create_text(self.x+self.w//2, self.y+self.h//2, text=self.text, font=self.font, fill="white")
+    def changePos(self, x, y):
+        self.x += x
+        self.y += y
+        self.points = [(self.x+self.marge, self.y), (self.x+self.w-self.marge, self.y), (self.x+self.w, self.y+self.marge), (self.x+self.w, self.y+self.h-self.marge), (self.x+self.w-self.marge, self.y+self.h), (self.x+self.marge, self.y+self.h), (self.x, self.y+self.h-self.marge), (self.x, self.y+self.marge)]
+        self.cnv.delete(self.polygon)
+        self.cnv.delete(self.label)
+        self.polygon = self.cnv.create_polygon(self.points, outline="white", fill='')
+        self.label = self.cnv.create_text(self.x + self.w//2, self.y+self.h//2, text=self.text, font=self.font, fill="white")
 
-        def bind(self, event, func):
-                self.cnv.tag_bind(self.polygon, event, func)
-                self.cnv.tag_bind(self.label, event, func)
-                self.events.append((event, func))
+    def centerLabel(self):
+        self.cnv.delete(self.label)
+        self.label = self.cnv.create_text(self.x+self.w//2, self.y+self.h//2, text=self.text, font=self.font, fill="white")
 
-        def unbind(self, event):
+    def bind(self, event, func):
+            self.cnv.tag_bind(self.polygon, event, func)
+            self.cnv.tag_bind(self.label, event, func)
+            self.events.append((event, func))
+
+    def unbind(self):
+        for event, func in self.events:
             self.cnv.tag_unbind(self.polygon, event)
             self.cnv.tag_unbind(self.label, event)
 
-        def rebind(self):
-            for event, func in self.events:
-                self.cnv.tag_bind(self.polygon, event, func)
-                self.cnv.tag_bind(self.label, event, func)
+    def rebind(self):
+        """
+        apres le changement de pos le bouton n'est plus bind.
+        pour palier a cela la fonction rebind permet de redonner les evenements au bouton
+        """
+        for event, func in self.events:
+            self.cnv.tag_bind(self.polygon, event, func)
+            self.cnv.tag_bind(self.label, event, func)
 
-        def mouseOver(self, event):
-            self.cnv.itemconfig(self.polygon, fill="#888888")
+    def mouseOver(self, event):
+        self.cnv.itemconfig(self.polygon, fill="#888888")
 
-        def mouseQuit(self, event):
-            self.cnv.itemconfig(self.polygon, fill='')
+    def mouseQuit(self, event):
+        self.cnv.itemconfig(self.polygon, fill='')
 
-
-WIDTH = 1000
-HEIGHT = WIDTH * 9 // 14
-app = App()
-app.mainloop()
+if __name__ == "__main__":
+    WIDTH = 1000
+    HEIGHT = WIDTH * 9 // 14
+    app = App()
+    app.mainloop()
